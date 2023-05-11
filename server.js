8362fd76cb4c733429e36563e9985dbecdfb0dd9
Sumit chauhan <sumitchauhan9807666@gmail.com>
2023-05-11 22:51:29 +0530

    sdf

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.post("/consumer", async ({ body }, res) => {
     });
     const desc = new webrtc.RTCSessionDescription(body.sdp);
     await peer.setRemoteDescription(desc);
-    console.log(senderStream,"senderStream")
+    console.log(senderStream.getTracks(),"senderStream")
     senderStream.getTracks().forEach(track => peer.addTrack(track, senderStream));
     const answer = await peer.createAnswer();
     await peer.setLocalDescription(answer);
