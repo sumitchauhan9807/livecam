ed5a9fea6566c4769e484f75944e17054c151c0f
Sumit chauhan <sumitchauhan9807666@gmail.com>
2023-05-12 12:58:52 +0530

    sdf

--- public/script.js
+++ public/script.js
@@ -28,25 +28,25 @@ var peer = new Peer({
   path: '/peerjs',
   config: {
     'iceServers': [
-      { url: 'stun:stun01.sipphone.com' },
-      { url: 'stun:stun.ekiga.net' },
-      { url: 'stun:stunserver.org' },
-      { url: 'stun:stun.softjoys.com' },
-      { url: 'stun:stun.voiparound.com' },
-      { url: 'stun:stun.voipbuster.com' },
-      { url: 'stun:stun.voipstunt.com' },
-      { url: 'stun:stun.voxgratia.org' },
-      { url: 'stun:stun.xten.com' },
-      {
-        url: 'turn:192.158.29.39:3478?transport=udp',
-        credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-        username: '28224511:1379330808'
-      },
-      {
-        url: 'turn:192.158.29.39:3478?transport=tcp',
-        credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-        username: '28224511:1379330808'
-      }
+      // { url: 'stun:stun01.sipphone.com' },
+      // { url: 'stun:stun.ekiga.net' },
+      // { url: 'stun:stunserver.org' },
+      // { url: 'stun:stun.softjoys.com' },
+      // { url: 'stun:stun.voiparound.com' },
+      // { url: 'stun:stun.voipbuster.com' },
+      // { url: 'stun:stun.voipstunt.com' },
+      // { url: 'stun:stun.voxgratia.org' },
+      // { url: 'stun:stun.xten.com' },
+      // {
+      //   url: 'turn:192.158.29.39:3478?transport=udp',
+      //   credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+      //   username: '28224511:1379330808'
+      // },
+      // {
+      //   url: 'turn:192.158.29.39:3478?transport=tcp',
+      //   credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+      //   username: '28224511:1379330808'
+      // }
     ]
   },
 
