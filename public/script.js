1a4d833204655f9688551ca9daa63e1acde374e6
Sumit chauhan <sumitchauhan9807666@gmail.com>
2023-05-12 14:52:16 +0530

    sdf

--- public/script.js
+++ public/script.js
@@ -28,25 +28,29 @@ var peer = new Peer({
   path: '/peerjs',
   config: {
     'iceServers': [
-      // { url: 'stun:stun01.sipphone.com' },
-      // { url: 'stun:stun.ekiga.net' },
-      // { url: 'stun:stunserver.org' },
-      // { url: 'stun:stun.softjoys.com' },
-      // { url: 'stun:stun.voiparound.com' },
-      // { url: 'stun:stun.voipbuster.com' },
-      // { url: 'stun:stun.voipstunt.com' },
-      // { url: 'stun:stun.voxgratia.org' },
-      // { url: 'stun:stun.xten.com' },
-      // {
-      //   url: 'turn:192.158.29.39:3478?transport=udp',
-      //   credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-      //   username: '28224511:1379330808'
-      // },
-      // {
-      //   url: 'turn:192.158.29.39:3478?transport=tcp',
-      //   credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-      //   username: '28224511:1379330808'
-      // }
+      {
+        urls: "stun:a.relay.metered.ca:80",
+      },
+      {
+        urls: "turn:a.relay.metered.ca:80",
+        username: "b713400626ccb30ffe25c95a",
+        credential: "Plt4+JLao80tCGm8",
+      },
+      {
+        urls: "turn:a.relay.metered.ca:80?transport=tcp",
+        username: "b713400626ccb30ffe25c95a",
+        credential: "Plt4+JLao80tCGm8",
+      },
+      {
+        urls: "turn:a.relay.metered.ca:443",
+        username: "b713400626ccb30ffe25c95a",
+        credential: "Plt4+JLao80tCGm8",
+      },
+      {
+        urls: "turn:a.relay.metered.ca:443?transport=tcp",
+        username: "b713400626ccb30ffe25c95a",
+        credential: "Plt4+JLao80tCGm8",
+      },
     ]
   },
 
