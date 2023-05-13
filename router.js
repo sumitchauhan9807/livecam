4f787a1a8d69030c868659a110ab6f8d70ef4018
Sumit chauhan <sumitchauhan9807666@gmail.com>
2023-05-13 20:36:52 +0530

    asd

--- router.js
+++ router.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const userAuth = require('./middlewares/userAuth')
+const jwt = require("jsonwebtoken");
+
+
 router.get('/',userAuth,(req,res,next)=>{
   res.send("herrrlo")
+})
+
+
+router.get('/token',(req,res,next)=>{
+  let token = jwt.sign({ userId: 1324 }, process.env.JWT_KEY)
+  res.send(token)
 })
 
 module.exports = router;
